b5bf2968413aedfc6357ff8da0dce37e40f5deea
Villera69 <william.ramsfeldt@gmail.com>
2022-09-28 14:41:19 +0200

    first commit JAVA TAMAGOTCHI

--- src/Tamagotchi.java
+++ src/Tamagotchi.java
@@ -1,4 +1,54 @@
+import java.util.List;
+import java.util.Random;
+
 public class Tamagotchi {
+
+    int hunger;
+    int boredom;
+    List<String> words;
+    boolean isAlive;
+    Random generator;
+    public String name;
+
+    public void Tick() {
+        hunger++;
+        boredom++;
+
+        if (boredom > 10 && hunger > 10) {
+            isAlive = false;
+        }
+    }
+
+    public void Feed() {
+        hunger -= 5;
+    }
+
+    public void ReduceBoredom() {
+        boredom += 2;
+    }
+
+    public void Hi() {
+
+        if (words.size() == 0) {
+            System.out.println("Sorry, your tamagotchi has not learned any words.");
+        }
+
+        System.out.println(words.get(generator.nextInt(0, words.size())));
+        ReduceBoredom();
+    }
+
+    public void Teach(String word) {
+        words.add(word);
+        ReduceBoredom();
+    }
+
+    public void PrintStats() {
+        System.out.println(" " + name + "is alive || Hunger " + hunger + " || Boredom " + boredom + " || ");
+    }
+
+    public boolean GetAlive() {
+        return isAlive;
+    }
 
 
 }
